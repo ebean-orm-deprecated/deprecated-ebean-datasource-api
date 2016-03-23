e8941662c98ce9f834e07e706b325215a7f49f86
Robin Bygrave <robin.bygrave@gmail.com>
2016-03-24 01:01:12 +1300

    No effective change - Javadoc

--- src/main/java/org.avaje.datasource/DataSourcePool.java
+++ src/main/java/org.avaje.datasource/DataSourcePool.java
@@ -3,15 +3,27 @@ package org.avaje.datasource;
 import javax.sql.DataSource;
 
 /**
- * Created by rob on 23/03/16.
+ * DataSource pool API.
  */
 public interface DataSourcePool extends DataSource {
 
-  boolean getAutoCommit();
+  /**
+   * Return true if the pool defaults to using autocommit.
+   */
+  boolean isAutoCommit();
 
+  /**
+   * Shutdown the pool with the option to deregister the driver.
+   */
   void shutdown(boolean deregisterDriver);
 
+  /**
+   * Return the Runnable to execute for heartbeat testing.
+   */
   Runnable getHeartbeatRunnable();
 
+  /**
+   * Return the frequency to run the heart bean test.
+   */
   int getHeartbeatFreqSecs();
 }
